bc1ab1aa0664621bbba1d0070f163ca81487355a
BIJUMNDL <66832758+BIJUMNDL@users.noreply.github.com>
2021-06-21 05:48:33 +0530

    Update MULTIPLE_PING.py

--- MULTIPLE_PING.py
+++ MULTIPLE_PING.py
@@ -1,8 +1,8 @@
 
 from pythonping import ping
 
-for i in range(3):
-    for j in range(3):
+for i in range(255):
+    for j in range(255):
         ipaddresss=str('192.168.%s.%s'%(i,j))
         ping(ipaddresss,count=1,verbose=True)
 
